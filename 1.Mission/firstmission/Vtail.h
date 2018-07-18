60cdc6177da4761bd653e8f704f2fdd4ea95fa61
Yemre0 <40735356+Yemre0@users.noreply.github.com>
2018-07-18 16:39:55 +0300

    Vtail i yanlış göndermişim
    .

--- 1.Mission/firstmission/Vtail.h
+++ 1.Mission/firstmission/Vtail.h
@@ -1,40 +1,32 @@
 #ifndef Vtail
 #define Vtail
+#include "PWM_OUT.h"
+#include "PWM_IN.h"
 
-
-#include <Arduino.h>
-#include <PWM_IN.h>
-#include <PWM_OUT.h>
-
-
-
-#define Vtail_elevator_left 
-#define Vtail_elevator_right
-
-
-
-//eeeeee ?
-
-
-
-
-
+#define Throttle (A2) // Throttle read pin Arduino (A2)//*********
+#define Aileron (A3) //Aileron read pin Arduino (A3)
+#define Elevator (A4) //Elevator read pin Arduino (A4)//PWM_IN kütüphanesinden gelen değerler
+#define Rudder (A5) //Rudder read pin Arduino (A5)
+#define Drop (A6) //Drop read pin Arduino (A6)
+#define Flap (A7)//Flap read pin Arduino (A7)//*******************
+#define stabil durum 1500
+int gelen deger,sonuc; 
+if (stabil durum=Elevator && stabil durum=Rudder){
+  |(gelen deger-1500)|*0.25=sonuc;
+  Elevator=1500-sonuc;
+  Rudder=1500+sonuc;
+}
 
 
 
 
+//*******************************
+//Functions for Vtail
+//*******************************
+Vtail_StatusTypeDef PWM_OUT_Vtail_Start(struct __Vtail_HandleTypeDef *Vtail);
+Vtail_StatusTypeDef Vtail_Start(struct __PWM_IN_HandleTypeDef *PWM_In);
+Vtail_StatusTypeDef Vtail_Start(struct __PWM_OUT_HandleTypeDef *PWM_Out);
+Vtail_StatusTypeDef PWM_OUT_Vtail_Setup(struct __Vtail_HandleTypeDef *PWM_Out);
 
 
-
-
-
-
-
-
-
-
-Vtail_StatusTypeDef Vtail_Start(struct __Vtail_HandleTypeDef *tuygun_vtail)
-Vtail_StatusTypeDef Vtail_Setup(struct __Vtail_HandleTypeDef *tuygun_vtail);
-
-
-#endif 
+#endif Vtail
